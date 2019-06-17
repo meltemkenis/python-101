6473dbd338cc9129e787ce4c267dc06b9a25b976
Meltem <kenis.meltem@gmail.com>
2019-06-17 14:31:31 +0300

    word_counter

---
+++ word_counter.py
@@ -0,0 +1,8 @@
+
+print("Please enter a sentence: ")
+sentence = str(input())
+
+#Words are separated by space:
+words = sentence.split(" ")
+
+print("Number of words in this sentence: ", len(words))
